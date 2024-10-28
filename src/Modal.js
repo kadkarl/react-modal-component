1c545bbc7d0c821d65a29e1ee0c086adfa8e8926
StudioKad <studiokad75@gmail.com>
2024-10-28 14:13:21 +0100

    correction erreur Modal.js

--- src/Modal.js
+++ src/Modal.js
@@ -5,24 +5,22 @@ const Modal = ({ isOpen, onClose, title, children }) => {
   if (!isOpen) return null;
 
   return (
-    <>
-      <div className="modal-overlay">
-        <div className="modal-container">
-          <div className="modal-header">
-            <h2>{title}</h2>
-            <button className="modal-close" onClick={onClose}>
-              X
-            </button>
-          </div>
-          <div className="modal-content">{children}</div>
-          <div className="modal-footer">
-            <button className="btn-close" onClick={onClose}>
-              Close
-            </button>
-          </div>
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <div className="modal-header">
+          <h2>{title}</h2>
+          <button className="modal-close" onClick={onClose}>
+            X
+          </button>
+        </div>
+        <div className="modal-content">{children}</div>
+        <div className="modal-footer">
+          <button className="btn-close" onClick={onClose}>
+            Close
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
