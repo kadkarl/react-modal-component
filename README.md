691c49c262eac8d9845010c22340683aace20b28
StudioKad <studiokad75@gmail.com>
2024-10-28 12:15:34 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # react-modal-component
+
+## Installation
+
+```bash
+npm install react-modal-component
+```
+
+### Tests :
+
+Pour exécuter les tests unitaires avec Jest, utilise la commande suivante :
+
+```bash
+npm test
+```
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    library: "react-modal-component",
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM",
+    },
+  },
+};
